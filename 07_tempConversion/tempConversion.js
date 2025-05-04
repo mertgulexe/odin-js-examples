d389491a3ed687aa0dba454a2e81d76f18db1984
mertgulexe <gul.mert89@gmail.com>
2025-05-04 14:51:33 +0100

    Update finished functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreeFahrenheit) {
+    const degreeCelsius = (degreeFahrenheit - 32) * (5/9);
+    return parseFloat(degreeCelsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreeCelsius) {
+    const degreeFahrenheit = degreeCelsius * (9/5) + 32;
+    return parseFloat(degreeFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
