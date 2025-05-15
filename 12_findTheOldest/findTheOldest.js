fccb7abaffca3a4d3ba60f14fa720dd0e3b3b79f
mertgulexe <gul.mert89@gmail.com>
2025-05-15 13:08:20 +0100

    Fill the empty exercise function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleList) {
+    const CURRENT_YEAR = new Date().getFullYear();
+    const modifiedPeopleList = peopleList.map(
+        people => {
+            people.yearOfDeath ??= CURRENT_YEAR;
+            people.age = people.yearOfDeath - people.yearOfBirth;
+            return people;
+        }
+    );
+    
+    modifiedPeopleList.sort(
+        (a, b) => {
+            return a.age - b.age;
+        }
+    ).reverse();
+
+    return modifiedPeopleList[0];
 
 };
 
