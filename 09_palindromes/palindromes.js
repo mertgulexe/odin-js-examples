020a4f06a8965caccd000dada0ece76ae36c829d
mertgulexe <gul.mert89@gmail.com>
2025-05-15 11:09:23 +0100

    Populate empty exercise function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const cleanString = string.replace(/[^a-z0-9]+/gi, '').toLowerCase();
+    const reversedString = cleanString.split('').reverse().join('');
+    return cleanString === reversedString;
 };
 
 // Do not edit below this line
