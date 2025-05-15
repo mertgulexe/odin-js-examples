84e68ca039d351b31368333808a10aa55973635f
mertgulexe <gul.mert89@gmail.com>
2025-05-15 10:57:21 +0100

    Populate exercise functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(...num) {
+	return num.reduce((i, j) => (i + j));
 };
 
-const subtract = function() {
-	
+const subtract = function(...num) {
+		return num.reduce((i, j) => (i - j));
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce(
+        (i, j) => (i + j),
+        0
+    );
 };
 
-const multiply = function() {
-
+const multiply = function(num) {
+    return num.reduce((i, j) => (i * j));
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+    // my verbose solution
+    let accumulatedNum = 1;
+    if (!num || num === 1) {
+        return 1;
+    } else {
+        for (let n = 2; n <= num; n++) {
+            accumulatedNum *= n; 
+        }
+        return accumulatedNum;
+    }
+
+    // odin's solution, which has more concise logic
+    let product = 1;
+    for (let n = num; n > 0; n--) {
+        product *= n; 
+    }
+    return product;
 };
 
 // Do not edit below this line
