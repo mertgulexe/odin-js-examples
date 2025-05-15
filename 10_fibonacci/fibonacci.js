1f7d2f8fad8fffe832a340e0a985e284145dc94f
mertgulexe <gul.mert89@gmail.com>
2025-05-15 11:50:43 +0100

    Populate the exercise function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fibArr = [0, 1];
+    let sumNum;
+    const finalFibNum = typeof num !== "number" ? parseInt(num) : num;
 
+    if ([0, 1].includes(finalFibNum)) {
+        return finalFibNum
+    } else if (finalFibNum < 0) {
+        return "OOPS";
+    };
+
+    for (let i = 2; i <= finalFibNum; i++) {
+        sumNum = fibArr[0] + fibArr[1];
+        fibArr.splice(2, 1, sumNum);
+        fibArr[0] = fibArr[1];
+        fibArr[1] = sumNum;
+    }
+    return sumNum;
 };
 
 // Do not edit below this line
